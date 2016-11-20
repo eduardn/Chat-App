2e33a7998c5915b4312025acebce69fa2f3be95b
Costi <costin.badea.93@gmail.com>
2016-11-20 14:33:12 +0200

    push add user

--- app/components/chat/chat.controller.js
+++ app/components/chat/chat.controller.js
@@ -5,19 +5,22 @@
     'use strict';
 
     angular.module('chatApp')
-        .controller('ChatController',ChatController);
+        .controller('ChatController',ChatController)
+        .directive('createRoom', function() {
+            return {
+               // controller: 'ChatController',
+                templateUrl: 'components/directives/createRoom.html'
+            };
+        })
 
-    ChatController.$inject=['$scope', '$state', 'loginService', '$localStorage'];
+    ChatController.$inject=['$scope', '$state', 'loginService', '$localStorage', '$firebaseArray','$firebaseObject','$timeout','$rootScope','$q'];
 
-    function ChatController($scope, $state,loginService, $localStorage){
+
+    function ChatController($scope, $state,loginService, $localStorage, $firebaseArray, $firebaseObject, $timeout, $rootScope, $q){
 
         $scope.$storage = $localStorage.$default();
-
-        $scope.username = $scope.$storage.loggedUsername;
-
-
-
-        console.log("loggedUsername: ", $scope.username)
+        $scope.userName = $scope.$storage.loggedUsername;
+        console.log("loggedUsername: ", $scope.userName)
 
         $scope.logout = function(){
             console.log("Logged out");
@@ -26,6 +29,58 @@
         }
 
 
+    /*
+    * take all active rooms
+    * created on database
+    */
+    function listRooms(){
+            //var deferred = $q.defer();
+            firebase.database().ref('/rooms').once('value').then(function(snapshot) {
+                var room = snapshot.val();
+                console.log(room);
+                $scope.activeRooms = [];
+                $timeout(function(){ for(var dataRooms in room){
+                    $scope.activeRooms.push(room[dataRooms].roomName)
+                    console.log(room[dataRooms].roomName);
+                }}, 5);
+            });
     }
 
+
+        /*
+        *Call function to
+        *list users
+        */
+        listRooms();
+
+        /*
+        * https://firebase.google.com/docs/database/web/read-and-write
+        * adding a new room to database
+        * TO DO : RESOLVE WITH MESSAGES
+        */
+
+        $scope.writeRoomData = function(roomName) {
+            roomName = $scope.roomNameCreate;
+            console.log(roomName);
+            firebase.database().ref('rooms/' + roomName).set({
+                        "messageObj" : {
+                            "data" : "18/11/2016",
+                            "sender" : $scope.userName,
+                            "text" : "1st Message"
+                        },
+                        "roomName" : roomName,
+                        "users" : {
+                            "name" :$scope.userName
+                        }
+                });
+        /*
+        *Call again function to
+        *list rooms to refresh
+        *interface
+        */
+        listRooms();
+        }
+
+
+    }
 })();
