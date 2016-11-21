4b81a1abfa32b66c476040609a71ff3370d9235f
Andreea Ruxandra Tudor <ruxx.andreea@yahoo.in>
2016-11-21 09:43:22 +0200

    list chatrooms

--- app/components/chat/chat.controller.js
+++ app/components/chat/chat.controller.js
@@ -1,24 +1,36 @@
-/**
- * Created by bcojocariu on 11/3/2016.
- */
+
 (function(){
     'use strict';
+   
 
     angular.module('chatApp')
         .controller('ChatController',ChatController);
 
-    ChatController.$inject=['$scope', '$state'];
+    ChatController.$inject=['$scope', '$state','$firebaseArray','$timeout'];
 
-    function ChatController($scope, $state){
+    function ChatController($scope, $state,$firebaseArray, $timeout){
 
         $scope.name = "";
+        $scope.rooms = [];
+        $scope.roomsNames = [];
 
         $scope.logout = function(){
             console.log("Logged out");
             $state.go('home');
         }
 
+        var database = firebase.database();
 
+        database.ref('/rooms').once('value').then(function(snap){
+            $scope.rooms = snap.val();
+        
+            $timeout(function(){
+                for( var key in $scope.rooms){
+                $scope.roomsNames.push($scope.rooms[key].roomName);
+                }
+             console.log($scope.roomsNames);
+            },5);
+        });
     }
 
 })();
