23b6d225cdaf845e63ad9bb373998b53e9993c46
Anca Budaca <abudaca@endava.net>
2016-11-22 10:05:39 +0200

    count users

--- app/components/chat/chat.controller.js
+++ app/components/chat/chat.controller.js
@@ -1,29 +1,32 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('chatApp')
         .controller('ChatController', ChatController)
-        .directive('createRoom', function() {
+        .directive('createRoom', function () {
             return {
                 // controller: 'ChatController',
                 templateUrl: 'components/directives/createRoom.html'
             };
-        })
+        });
 
-    ChatController.$inject = ['$scope', '$state', 'loginService', '$localStorage', '$firebaseArray', '$firebaseObject', '$timeout', '$rootScope', '$q'];
+    ChatController.$inject = ['$scope', '$state', '$location', 'loginService', '$localStorage', '$firebaseArray', '$firebaseObject', '$timeout', '$rootScope', '$q'];
 
 
-    function ChatController($scope, $state, loginService, $localStorage, $firebaseArray, $firebaseObject, $timeout, $rootScope, $q) {
-
+    function ChatController($scope, $state, $location, loginService, $localStorage, $firebaseArray, $firebaseObject, $timeout, $rootScope, $q) {
 
         $scope.$storage = $localStorage.$default();
         $scope.userName = $scope.$storage.loggedUsername;
+        if (!$scope.userName) {
+            console.log("should redirect to login");
+            $location.path('/home');
+        }
         console.log("loggedUsername: ", $scope.userName);
         $scope.name = "";
         $scope.rooms = [];
 
 
-        $scope.logout = function() {
+        $scope.logout = function () {
             console.log("Logged out");
             $state.go('home');
             $scope.$storage = $localStorage.$reset();
@@ -36,10 +39,10 @@
         var database = firebase.database();
 
         function listRooms() {
-            database.ref('/rooms').once('value').then(function(snap) {
+            database.ref('/rooms').once('value').then(function (snap) {
                 $scope.rooms = snap.val();
                 $scope.roomsNames = [];
-                $timeout(function() {
+                $timeout(function () {
                     for (var key in $scope.rooms) {
                         $scope.roomsNames.push($scope.rooms[key].roomName);
                     }
@@ -49,15 +52,16 @@
 
         /* GAD team code */
         function listUsers() {
-            database.ref('/rooms').once('value').then(function(snap) {
+            database.ref('/rooms').once('value').then(function (snap) {
                 $scope.rooms = snap.val();
-                $timeout(function() {
+                $timeout(function () {
                     for (var key in $scope.rooms) {
                         console.log(key);
                     }
                 }, 5);
             });
         }
+
         listUsers();
 
 
@@ -66,19 +70,19 @@
          * created on database
          */
         /*  function listRooms() {
-              //var deferred = $q.defer();
-              firebase.database().ref('/rooms').once('value').then(function(snapshot) {
-                  var room = snapshot.val();
-                  console.log(room);
-                  $scope.activeRooms = [];
-                  $timeout(function() {
-                      for (var dataRooms in room) {
-                          $scope.activeRooms.push(room[dataRooms].roomName)
-                          console.log(room[dataRooms].roomName);
-                      }
-                  }, 5);
-              });
-          }*/
+         //var deferred = $q.defer();
+         firebase.database().ref('/rooms').once('value').then(function(snapshot) {
+         var room = snapshot.val();
+         console.log(room);
+         $scope.activeRooms = [];
+         $timeout(function() {
+         for (var dataRooms in room) {
+         $scope.activeRooms.push(room[dataRooms].roomName)
+         console.log(room[dataRooms].roomName);
+         }
+         }, 5);
+         });
+         }*/
 
 
         /*
@@ -93,7 +97,7 @@
          * TO DO : RESOLVE WITH MESSAGES
          */
 
-        $scope.writeRoomData = function(roomName) {
+        $scope.writeRoomData = function (roomName) {
             console.log($scope.roomNameCreate);
             roomName = $scope.roomNameCreate;
             console.log(roomName);
@@ -112,35 +116,47 @@
              *list rooms to refresh
              *interface
              */
-              $scope.roomNameCreate = null;
+            $scope.roomNameCreate = null;
             listRooms();
-          
+
+
         }
+        //    count users in a room
+        $scope.getCount = function (users) {
+            var i = 0;
+            for (var p in users) {
+                if (users.hasOwnProperty(p)) {
+                    i++
+                }
+            }
+            return i
+
+        };
 
         /*
          *Join a room as
          *user, from the available rooms
          */
-        $scope.joinRoom = function(room) {
+        $scope.joinRoom = function (room) {
 
-                firebase.database().ref('rooms/' + room + '/users/').push($scope.userName);
-                database.ref('rooms/' + room + '/count').once('value').then(function(snap) {
-                    //console.log(snap.val());
-                    // firebase.database('rooms/' + room + '/count').ref().update(updates)
-                });
+            firebase.database().ref('rooms/' + room + '/users/').push($scope.userName);
+            database.ref('rooms/' + room + '/count').once('value').then(function (snap) {
+                //console.log(snap.val());
+                // firebase.database('rooms/' + room + '/count').ref().update(updates)
+            });
 
-            }
-            /*TESTING DATA FOR ROOM USERS
+        }
+        /*TESTING DATA FOR ROOM USERS
 
-               firebase.database().ref('rooms/'+ 'room2' +'/users/').once('value').then(function(snapshot) {
-                    var data = snapshot.val();
-                    console.log(data);
+         firebase.database().ref('rooms/'+ 'room2' +'/users/').once('value').then(function(snapshot) {
+         var data = snapshot.val();
+         console.log(data);
 
-                    for(var key in data ){
-                        console.log(data[key]);
-                    }
+         for(var key in data ){
+         console.log(data[key]);
+         }
 
-                });
-                */
+         });
+         */
     }
 })();
