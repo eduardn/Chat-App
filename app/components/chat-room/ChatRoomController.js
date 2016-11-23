07987d70d1d371fce3c82bb72362fa8210462646
Georgiana Ilinca <gilinca@endava.com>
2016-11-23 12:01:42 +0200

    new

--- app/components/chat-room/ChatRoomController.js
+++ app/components/chat-room/ChatRoomController.js
@@ -2,6 +2,22 @@
     'use strict';
 
     angular.module('chatApp')
+
+    .directive('schrollBottom', function () {
+  return {
+    scope: {
+      schrollBottom: "="
+    },
+    link: function (scope, element) {
+      scope.$watchCollection('schrollBottom', function (newValue) {
+        if (newValue)
+        {
+          $(element).scrollTop($(element)[0].scrollHeight);
+        }
+      });
+    }
+  }
+})
         .controller('ChatRoomController', ChatRoomController);
 
     ChatRoomController.$inject = ['$scope', '$state', '$firebaseArray', '$firebaseObject', '$rootScope', '$q', '$stateParams'];
